abe2a1a5d8005c2a2059c22483b9831f0ea99351
Ted Smongeski <t3dw4rd0@gmail.com>
2021-05-13 22:49:22 -0400

    more stuff

---
+++ dot-emacs.d/init.el
@@ -0,0 +1,11 @@
+;; WIP WIP WIP WIP
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
