949256936615daf7592e6794bf9b180f0282733e
Ted Smongeski <esmongeski@wayfair.com>
2021-05-20 12:05:29 -0400

    more docs

--- bash_profile.bash
+++ bash_profile.bash
@@ -1,2 +1,4 @@
+# Looking for a bunch of fun bash shortcuts? Check out ./bashProfile.org
+
 bash $PATH_TO_DOTFILES_REPO/generateBashProfile.bash > /dev/null 2>&1
 . $PATH_TO_DOTFILES_REPO/bashProfile.bash
