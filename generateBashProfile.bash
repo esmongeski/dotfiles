d4c3ca2964cf0f213e7c2603655eb195927f87f5
Edward Smongeski <edward.smongeski@gmail.com>
2021-05-20 08:48:00 -0400

    Move bash profile to org

--- generateBashProfile.bash
+++ generateBashProfile.bash
@@ -2,5 +2,5 @@ emacs --batch \
       --eval "(require 'org)" \
       --eval "(org-babel-do-load-languages 'org-babel-load-languages'((shell . t)))" \
       --eval "(setq org-babel-default-header-args:bash '((:tangle . \"yes\")))" \
-      --eval '(org-babel-tangle-file "./bashProfile.org")'
+      --eval '(org-babel-tangle-file (concat (getenv "PATH_TO_DOTFILES_REPO") "/bashProfile.org"))'
 
