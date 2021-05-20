75f50b2efb58a2515ab324a50ec96b10e89e7482
Edward Smongeski <edward.smongeski@gmail.com>
2021-05-19 22:31:14 -0400

    WIP - migrating bash profile to org

---
+++ generateBashProfile.bash
@@ -0,0 +1,6 @@
+emacs --batch \
+      --eval "(require 'org)" \
+      --eval "(org-babel-do-load-languages 'org-babel-load-languages'((shell . t)))" \
+      --eval "(setq org-babel-default-header-args:bash '((:tangle . \"yes\")))" \
+      --eval '(org-babel-tangle-file "./bashProfile.org")'
+
