5eeba4aadb63301d9628fe31ca7893d34b8ef6f5
Edward Smongeski <edward.smongeski@gmail.com>
2021-08-01 22:12:30 -0400

    More fish config

--- fish/fish_profile.fish
+++ fish/fish_profile.fish
@@ -1,6 +1,4 @@
 # Looking for a bunch of fun bash shortcuts? Check out ./fishProfile.org
 
-echo "bonjour"
-
 bash $PATH_TO_DOTFILES_REPO/fish/generateFishProfile.bash > /dev/null 2>&1
 . $PATH_TO_DOTFILES_REPO/fish/fishProfile.fish
